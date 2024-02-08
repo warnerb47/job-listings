dd820e94823f08917b7125fe0986861989bf9b54
warnerb47 <taphagaye2001.mg@gmail.com>
2024-02-08 18:06:12 +0000

    feat: init job component

--- src/app/job-list/job-list.component.ts
+++ src/app/job-list/job-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterComponent } from '../filter/filter.component';
+import { JobComponent } from '../job/job.component';
 
 @Component({
   selector: 'job-listings-job-list',
   standalone: true,
-  imports: [CommonModule, FilterComponent],
+  imports: [CommonModule, FilterComponent, JobComponent],
   templateUrl: './job-list.component.html',
   styleUrl: './job-list.component.scss',
 })
---
+++ src/app/job/job.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'job-listings-job',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './job.component.html',
+  styleUrl: './job.component.scss',
+})
+export class JobComponent {}
