b456e6a21c07bec626cf90c3b0f6311d78151fbc
warnerb47 <taphagaye2001.mg@gmail.com>
2024-02-08 15:33:19 +0000

    setup: add release please

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [],
   selector: 'job-listings-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
