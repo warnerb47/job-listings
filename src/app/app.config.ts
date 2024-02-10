39b70fc18c34db7ffcbfe5402c5e1570759c06ec
warnerb47 <taphagaye2001.mg@gmail.com>
2024-02-10 08:20:05 +0000

    setup: inject Job Gateway config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { JobGateway } from './core/ports/job.gateway';
+import { InMemoryJobGateway } from './core/adapters/in-memory-job.gateway';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    {provide: JobGateway, useValue: new InMemoryJobGateway()}
+  ],
 };
