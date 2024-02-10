e131021420bfcdecff8701078297ccc6d80a5e01
warnerb47 <taphagaye2001.mg@gmail.com>
2024-02-10 13:42:51 +0000

    refactor: add environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { JobGateway } from './core/ports/job.gateway';
 import { InMemoryJobGateway } from './core/adapters/in-memory-job.gateway';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from 'src/environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
-    {provide: JobGateway, useValue: new InMemoryJobGateway()}
+    {provide: JobGateway, useValue: new InMemoryJobGateway()},
+    importProvidersFrom(
+      NgxsModule.forRoot([], {
+        developmentMode: !environment.production
+      })
+    ),
   ],
 };
