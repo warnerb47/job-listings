9acc9a3b3da911e65dec1af7e58470ec98096e5d
warnerb47 <taphagaye2001.mg@gmail.com>
2024-02-10 08:19:26 +0000

    feat: add in memory gateway

---
+++ src/app/core/adapters/in-memory-job.gateway.ts
@@ -0,0 +1,28 @@
+import { Observable, map, of } from "rxjs";
+import { Job } from "../models/job.model";
+import { JobGateway } from "../ports/job.gateway";
+import data from "../../../assets/json/data.json";
+
+export class InMemoryJobGateway extends JobGateway {
+    
+    jobs: Job[] = data;
+
+    override retrieveAll(): Observable<Job[]> {
+        return of(this.jobs);
+    }
+
+    override filter(query: string[]): Observable<Job[]> {
+        return of(this.jobs).pipe(
+            map(jobs => jobs.filter(j => {
+                const includeLanguage  = j.languages.some( l => query.includes(l));
+                const includeTools = j.tools.some(t => query.includes(t));
+                return (
+                    query.includes(j.role)
+                    || query.includes(j.level)
+                    || includeLanguage
+                    || includeTools
+                );
+            }))
+        )
+    }
+}
