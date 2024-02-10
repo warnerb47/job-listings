472de77a5f6b02c08876023ebab1e8151f2c4c57
warnerb47 <taphagaye2001.mg@gmail.com>
2024-02-10 15:24:40 +0000

    feat: add job builder

--- src/app/core/stores/job/job.state.spec.ts
+++ src/app/core/stores/job/job.state.spec.ts
@@ -1,13 +1,20 @@
 import { TestBed } from "@angular/core/testing";
 import { NgxsModule, Store } from "@ngxs/store";
 import { JobState } from "./job.state";
+import { InMemoryJobGateway } from "../../adapters/in-memory-job.gateway";
+import { JobGateway } from "../../ports/job.gateway";
 
 describe('JobState', () => {
     let store: Store
+    let jobGateway: InMemoryJobGateway;
 
     beforeEach(() => {
+        jobGateway = new InMemoryJobGateway();
         TestBed.configureTestingModule({
-            imports: [NgxsModule.forRoot([JobState])]
+            imports: [NgxsModule.forRoot([JobState])],
+            providers: [
+                { provide: JobGateway, useValue: jobGateway }
+            ],
         });
         store = TestBed.inject(Store);
     });
@@ -16,5 +23,10 @@ describe('JobState', () => {
         expect(store.snapshot().job).toEqual({
             jobs: []
         });
+    });
+
+
+    it('should retrieve jobs', () => {
+        jobGateway.withJobs([])
     });
 });
