75ec5f880a46a7793ada65bea3e672a55799dfce
warnerb47 <taphagaye2001.mg@gmail.com>
2024-02-12 15:40:31 +0000

    fix: fix job state configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,14 @@ import { JobGateway } from './core/ports/job.gateway';
 import { InMemoryJobGateway } from './core/adapters/in-memory-job.gateway';
 import { NgxsModule } from '@ngxs/store';
 import { environment } from 'src/environments/environment.development';
+import { JobState } from './core/stores/job/job.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     {provide: JobGateway, useValue: new InMemoryJobGateway()},
     importProvidersFrom(
-      NgxsModule.forRoot([], {
+      NgxsModule.forRoot([JobState], {
         developmentMode: !environment.production
       })
     ),
